7192f4a586b8b08d79023d48f82e2913aeefbb1f
James Ngandu <jamesmngandu@gmail.com>
2023-06-11 00:21:13 +0300

    added

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+
+
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers."""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
+
+        print("")
