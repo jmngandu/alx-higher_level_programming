5c7d0adeb17981d281161e0c9fcc6c02559e7c3f
James Ngandu <jamesmngandu@gmail.com>
2023-06-15 13:11:52 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+# 10-best_score.py
+
+
 def best_score(a_dictionary):
-    """find the max of a dictionary"""
-    if not a_dictionary:
+    """Returns a key with the biggest integer value.
+    Checks if a thing is a dicrionary with the is instance method"""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    return max(a_dictionary, key=a_dictionary.get())
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
