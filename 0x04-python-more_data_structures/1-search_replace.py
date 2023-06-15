ba574cac72caf8aefed20932b9bf33ae3fd05821
James Ngandu <jamesmngandu@gmail.com>
2023-06-15 12:47:14 -0700

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    return ([item if item != search else replace for item in my_list])
-Footer
+# 1-search_replace.py
 
+
+def search_replace(my_list, search, replace):
+    """Replace all occurrences of an element by another in a new list."""
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
