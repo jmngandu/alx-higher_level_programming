c69cd3c89cd64ffc397b23d8c01e41dbb45c14c4
James Ngandu <jamesmngandu@gmail.com>
2023-06-15 13:19:43 -0700

    Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 100-weight_average.py
+
+
+def weight_average(my_list=[]):
+    """Return the weighted average of all integers in a list of tuples."""
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+
+    avg = 0
+    size = 0
+    for tup in my_list:
+        avg += (tup[0] * tup[1])
+        size += tup[1]
+    return (avg / size)
