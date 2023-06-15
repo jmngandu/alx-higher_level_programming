f1135cfc5178acc7cba5459faec732fddb42876e
James Ngandu <jamesmngandu@gmail.com>
2023-06-15 13:00:26 -0700

    Print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 6-print_sorted_dictionary.py
+
+
+def print_sorted_dictionary(a_dictionary):
+    """Print a dictionary by ordered keys."""
+    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
