78969bd655072cb3e902c36a7911634a90908f06
James Ngandu <jamesmngandu@gmail.com>
2023-06-15 13:02:23 -0700

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+# 7-update_dictionary.py
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
