9fb747e6aa868dbb835c77f21ab9f01ea3ac2ed6
James Ngandu <jamesmngandu@gmail.com>
2023-06-27 12:03:15 -0700

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 2-square.py
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            __size (int): The __size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
