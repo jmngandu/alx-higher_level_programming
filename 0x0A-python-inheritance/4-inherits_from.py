6f78c31fef784eb9277db8d4fba738420e0daefc
jmngandu <jamesmngandu@gmail.com>
2024-06-03 22:36:25 +0300

    Python - Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-
-
-""" Module to assert if an object is an instance
-of a class that inherited from the specifed classs
+# 4-inherits_from.py
+"""Defines an inherited class-checking function.
 """
 
 
 def inherits_from(obj, a_class):
-    """Check if an object is an instance of class that inherits from a_class.
+    """Checks if an object is an inherited instance of a class.
     Args:
-        obj (any) ->  The object to check.
-        a_class (type) ->  The class to match the type of obj to.
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
     Returns:
-        True if class of obj inherits a_class; False Otherwise
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
     """
-    obj_class = type(obj)
-    return issubclass(obj_class, a_class) and type(obj) != a_class
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
