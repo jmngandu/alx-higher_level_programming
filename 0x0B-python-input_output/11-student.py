ab8be8f83b219dd3ca2a17c3d7b61d0818f682f8
jmngandu <jamesmngandu@gmail.com>
2024-06-04 09:48:25 +0300

    Python - Input/Output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,33 +1,44 @@
-#!/usr/bin/python3
-
-"""Student class to represent a student"""
+#!/usr/bin/pyton3
+"""Defiantion for a student class
+"""
 
 
 class Student:
-    """Defining student class """
-    def __init__(self, first_name, last_name, age):
-        """Student constructor
-        Args:
-            first_name(str)
-            last_name(str)
-            age(str)
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+   """a class for a student
+   """
 
-    def to_json(self, attrs=None):
-        """ Retrive information about an instance to json
-        Args:
-            attrs(list) -> List of attribute to be retrieved
-        """
-        if (type(attrs) == list and
-                all(type(element) == str for element in attrs)):
-            return {key: getattr(self, key)
-                    for key in attrs if hasattr(self, key)}
-        return self.__dict__
+   def __init__(self, first_name, last_name, age):
+       """Initalizes a student object
+       """
 
-    def reload_from_json(self, json):
-        """Replace attributes of the object with k-v pair in the json """
-        for key, value in json.items():
-            setattr(self, key, value)
+       self.first_name = first_name
+       self.last_name = last_name
+       self.age = age
+
+   def to_json(self, attrs=None):
+       """returns a json representatiion of
+       the object
+       """
+
+       if attrs != None and not isinstance(attrs, list):
+           raise TypeError("attrs must me a list of strings")
+
+       if attrs != None and any(not isinstance(attr, str) for attr in attrs):
+           raise TypeError("attrs must me a list of strings")
+
+       if attrs == None:
+           return self.__dict__
+
+       json_dic = {}
+       for key in attrs:
+           if key in self.__dict__.keys():
+               json_dic[key] = self.__dict__[key]
+
+       return json_dic
+
+    def relod_from_json(self, json):
+    """replaces object's attributes
+    """
+
+        for k, v in json.items():
+            setattr(self, k, v)
