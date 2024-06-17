02e313e04e34484cff070bad46b58534ad5d6b51
jmngandu <jamesmngandu@gmail.com>
2024-06-17 14:31:13 +0300

    JavaScript is amazing

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argument = 1;
+if (argument === 1) {
+  console.log('Argument found');
+} else if (argument === ' ') {
+  console.log('No argument');
+} else {
+  console.log('Arguments found');
+}
