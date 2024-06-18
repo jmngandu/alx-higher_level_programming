51db0894d984aa397c0abf1314ae5fc3482cfa26
jmngandu <jamesmngandu@gmail.com>
2024-06-18 23:06:15 +0300

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/101-call_me_moby.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// Executes theFunction 'x' times
-exports.callMeMoby = function (x, theFunction) {
-  while (x-- > 0) {
-    theFunction();
-  }
-};
--- 0x13-javascript_objects_scopes_closures/102-add_me_maybe.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/node
-exports.addMeMaybe = (x, func) => { func(x + 1); };
