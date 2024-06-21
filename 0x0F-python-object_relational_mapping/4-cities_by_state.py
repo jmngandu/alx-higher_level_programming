0a4a6f8483c8903b458d248f68ea98d744fef3a6
jmngandu <jamesmngandu@gmail.com>
2024-06-21 21:37:23 +0300

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,36 @@
-"""
 #!/usr/bin/python3
-# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
-# Usage: ./4-cities_by_state.py <mysql username> \
-#                               <mysql password> \
-#                               <database name>
+
+"""
+Lists all cities from the cities table of database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <username> \
+                            <password> \
+                            <database-name>
 """
 import sys
-import MySQLdb
+import MySQLdb as db
+
+
+def connect_and_query() -> None:
+
+    """Connect to the database and execute query"""
+    try:
+        cnx = db.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        cursor = cnx.cursor(cursorclass=db.cursors.Cursor)
+        cursor.execute('SELECT city.id, city.name, state.name\
+                        FROM cities as city\
+                        INNER JOIN states as state\
+                        ON city.state_id = state.id\
+                        ORDER BY city.id ASC;')
+        cities = cursor.fetchall()
+
+        for city in cities:
+            print(city)
+
+        cursor.close()
+        cnx.close()
+    except Exception as e:
+        return (e)
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
-                 FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    [print(city) for city in c.fetchall()]
+    connect_and_query()
