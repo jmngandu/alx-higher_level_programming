ed8a9789bff9e75970b89c7a3a13a0aa27908657
jmngandu <jamesmngandu@gmail.com>
2024-07-31 15:19:45 +0300

    javaScript - Web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,36 @@
 #!/usr/bin/node
+const request = require('request');
 
-const req = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-req.get(url + id, function (error, res, body) {
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, function (error, res, body1) {
-      if (error) {
-        console.log(error);
-      }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
+const movieId = process.argv[2];
+const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const fullUrl = baseUrl.concat(movieId);
+
+request(fullUrl, (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    // Create a variable to store the number of characters processed
+    let charactersProcessed = 0;
+    // Create an empty array to store the character names
+    const characterNames = [];
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
+        if (!error) {
+          const charName = JSON.parse(body).name;
+          // Add the character name to the array
+          characterNames.push(charName);
+        }
+        // Increment the charactersProcessed variable
+        charactersProcessed++;
+        // Check if all characters have been processed
+        if (charactersProcessed === characters.length) {
+          // Log the character names when all characters have been processed
+          characterNames.forEach((actor) => {
+            console.log(actor);
+          });
+        }
+      });
     });
+  } else {
+    console.log(error);
   }
 });
