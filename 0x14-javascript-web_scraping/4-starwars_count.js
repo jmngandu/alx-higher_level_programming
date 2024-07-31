f44e6b0a23d6fb3985ad39d3dddc1a20684d938d
jmngandu <jamesmngandu@gmail.com>
2024-07-31 15:07:55 +0300

    javaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (err, response, body) {
+  let count = 0;
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  for (let i = 0; data.results[i] !== undefined; i++) {
+    if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      count++;
+    }
+  }
+  console.log(count);
+});
